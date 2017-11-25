06d9b58267cce2ffa0e8af0a2322737c33cb5fa6
後閑諒一 <nargok@gmail.com>
2017-11-25 22:38:41 +0900

    access log & system log

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require("express");
+var accesslogger = require("./lib/logger/accesslogger.js");
+var systemlogger = require("./lib/logger/systemlogger.js");
 var app = express();
 
 app.set("view engine", "ejs");
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use(accesslogger());
+
 app.use("/", require("./routes/index.js"));
+
+app.use(systemlogger());
 
 app.listen(3000);
